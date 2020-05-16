10bbde158dbbc95d0eb16744a25bc0c01652b47d
kuresato <kuresato@gmail.com>
2020-05-16 18:06:32 +0900

    change application name

---
+++ src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(path = "/")
+	public Map<String, String> index() {
+		Map<String, String> map = new HashMap<>();
+		map.put("message", "hello-dx");
+		return map;
+	}
+
+}
+
--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.hello;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class HelloApplicationTests {
 
 	@Test
 	public void contextLoads() {
