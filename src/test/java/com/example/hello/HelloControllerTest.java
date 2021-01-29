e4ba92ee79bb50e55b426fa2e9c52fda26c07f03
kuresato <kuresato@gmail.com>
2021-01-29 16:29:56 +0900

    Controllerのテストにモックを追加

--- src/test/java/com/example/hello/HelloControllerTest.java
+++ src/test/java/com/example/hello/HelloControllerTest.java
@@ -4,19 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// @SpringBootTest
-// @AutoConfigureMockMvc
+//@SpringBootTest
+//@AutoConfigureMockMvc
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    private Message propMessage;
 
     @Test
     public void testHello() throws Exception {
